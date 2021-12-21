2282efccd8640e9c050d5ef03e201eedfb8a46bb
Felix <felixhopostma@gmail.com>
2021-12-21 10:39:00 +0100

    made sure to remove xml before initialising dicttree

--- test_dicttree.py
+++ test_dicttree.py
@@ -1,10 +1,16 @@
 from genericpath import exists
+import os
+
 import dicttree
 
 
+
 xmlname = 'dicttree.xml'
+if exists(xmlname):
+    os.remove(xmlname)
+
 d = dicttree.DictTree(xmlname)
 
 def test_init():
-    assert exists(xmlname+'f')
+    assert exists(xmlname)
 
