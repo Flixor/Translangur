76b18bdd55c71e86f3f9ff3180742ad9c4c33de2
Felix <felixhopostma@gmail.com>
2021-12-21 10:38:32 +0100

    added commentary and template of remove() method

--- dicttree.py
+++ dicttree.py
@@ -1,22 +1,50 @@
 import os
+from genericpath import exists
 
 
 class DictTree (object):
+    """
+    The class defines a dictionary tree.
+    It's called a dictionary because it is used to store 
+    source phrases and their translations,
+    but it also has a tree structure, in the form of an xml file.
+    """
+
 
     def __init__(self, xmlname):
-        if not os.path.isfile(xmlname):
+        """
+
+        Parameters:
+            xmlname (str): The name of the xml file to use, 
+                and create if non-existent.
+
+        """
+        if not exists(xmlname):
+            # create file without needing to call close()
             with open(xmlname, 'w') as f:
                 pass
         
 
-    def add_phrase(self, source, tlation):
+    def add(self, src, tl):
         """
-        Adds a phrase and its translation to the dictionary.
+        Adds a source phrase and its translation phrase to the dicttree.
+        These phrases are assumed to be words (which format?),
+        separated by whitespaces and/or punctuation marks. 
+
+        The addition is destructive. Therefore, this method can also
+        be used to modify previous entries.
 
         Parameters:
-            source (str): The source phrase.
+            src (str): The source phrase.
             tl (str): The translations of the phrase.
 
+        """
+        pass
+
+
+    def remove(self, src):
+        """
+        Removes the translation of a source phrase from the dicttree.
         """
         pass
     
